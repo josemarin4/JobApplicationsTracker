ca5844c2b5fb0972b76d0da115b56e198443325f
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2025-02-20 15:07:30 -0500

    implement job application model

---
+++ src/main/java/app/jobapplicationtracker/model/JobApplication.java
@@ -0,0 +1,27 @@
+package app.jobapplicationtracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID applicationId;
+    private String companyName;
+    private String applicationTitle;
+    private Status status;
+    private LocalDate dateApplied;
+}
